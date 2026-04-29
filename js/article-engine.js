957aba4d11004e0f09c676bd33d512dc26b9960d
hank <lohaslife.hank@gmail.com>
2026-04-29 15:21:58 +0800

    Updates

--- js/article-engine.js
+++ js/article-engine.js
@@ -35,11 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="col-md-6 col-lg-4 article-item" data-reveal>
           <div class="card h-100 border-0 shadow-sm rounded-4 overflow-hidden">
             <div class="card-img-wrapper" style="aspect-ratio: 16/9; background: #f0f0f0;">
-              <img src="${article.image}">
-                   loading="lazy" 
-                   class="card-img-top" 
-                   style="width: 100%; height: 100%; object-fit: cover;" 
-                   alt="${article.title_zh}">
+            <img 
+              src="${article.image}" 
+              loading="lazy" 
+              class="card-img-top" 
+              alt="${article.title_zh}"
+            >
             </div>
   
             <div class="card-body p-4">
